d52c0093e61289a02860a47e9d554380a9307f79
Michael Truong <michaeltruong@Michaels-MacBook-Air.local>
2020-10-31 19:55:14 -0400

    landscaper: user can buy scissors

--- landscaper/app.js
+++ landscaper/app.js
@@ -1,6 +1,6 @@
 // start off with money 
 console.log("Game is running");
-let userAction = null; 
+let userAction = null; // use as killSwitch
 let totalEarned = 0; 
 let winAmount = 10;
 
@@ -22,34 +22,44 @@ let currTool = tools[0];
 while (userAction !== "stop" && totalEarned !== 10) { 
 
     // keep prompting to ask if want to keep cutting lawns? 
-    userAction = prompt("Do you want to keep cutting lawns?", "yes or no"); 
+    userAction = prompt("Do you want to keep cutting lawns?", "y or n"); 
 
         // if (userReply === yes) keep cutting! 
-        if (userAction === "yes") {
+        if (userAction === "y") {
         // use currTool.payout to increase totalEarned until it hits the following if-statement! 
         // totalEarned += currTool.payout;
             totalEarned += currTool.payout;
             console.log(totalEarned); 
         }
         // if (userReply === no) exit game and alert() Game Over! 
-        if (userAction === "no") {
+        if (userAction === "n") {
             alert("Game Over!"); 
+            console.log("Game has stopped running");
         }
              
 
-    // 3. if (currTool.name === "teeth" && totalEarned === 5) 
+    // 3. upgrade to 'rusty-scissors' 
 
-        // const userAction = prompt("You can now upgrade to the next tool!", "Yes or No?"); 
+    if (currTool.name === "teeth" && totalEarned === 5) {
+        // prompt() user to upgrade to nextTool! 
 
-            // if (userAction === "yes")
-
+        userAction = prompt(`You can now upgrade to the ${tools[1].name}!`, "y or n?"); 
+            // check if (yes) 
+            if (userAction === "y") {
                 // reassign currTool equal to next Tool-Object in the array! 
+                currTool = tools[1];
                 // THEN decrease totalEarned by the currTool.cost 
-                // totalEarned -= currTool.cost; 
-    
+                // totalEarned -= currTool.cost;
+                totalEarned -= currTool.cost;
+                console.log(totalEarned); 
+            }
+
+            // don't need an else-statement 
+    }
+
+
     // 4. copy and paste the logic for EVERY tool! START! 
                 
-
     // need FINAL WINNING STATEMENT 
     if (totalEarned === 10) {
         alert("You win!");
